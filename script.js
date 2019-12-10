629fb15c93c2f55db57a8e36c6e61c3a34e63535
Grothusen <grothusenmichaelw@johndeere.com>
2019-12-09 23:36:17 -0600

    added code to update the word spaces and alert the user if there was a match or not

--- script.js
+++ script.js
@@ -1,22 +1,48 @@
-// set the defualt word
+// set the defualt word with letter spaces
 const word = 'apple'
 let wordLgth = word.length
 console.log(wordLgth)
 for(let i = 0; i < wordLgth; i++){
-  let newElem = document.createElement("input")
-  document.querySelector("body").appendChild(newElem)
+  console.log(i)
+    let elm = document.querySelector('.wordspaces')
+    let newElem = document.createElement("input")
+
+    
+    let x = document.querySelector('.wordspaces input')
+    let newSpace = document.createAttribute("letterspace")
+    newSpace.value = i
+    elm.appendChild(newElem).setAttributeNode(newSpace)
+      
 }
-
-
-
-
-
 
 // caputure the letter the user inputed
 const button = document.querySelector('.button')
 button.addEventListener('click',captureLetter)
 
-function captureLetter(){
-  let inpLetter = document.querySelector("#letter")
-  console.log('this is the letter',inpLetter.value)
-}
+function captureLetter(e){
+ // console.log('this is the letter',inpLetter.value)
+  console.log(e)
+  let inpLetter = document.querySelector("#letter").value
+  console.log('input letter',inpLetter)
+  console.log('word length',wordLgth)
+  let count = 0
+  for(let i = 0; i < wordLgth; i++){
+    if(word.charAt(i) === inpLetter){
+        console.log(`this letter ${i} equals the input letter`)
+        document.querySelector(`[letterspace="${i}"]`).value = inpLetter
+        //document.querySelector(`[data-letter=${e.key}]`)
+        count = count + 1
+    }
+  }
+  console.log(count)
+  if(count > 0 ){
+    alert('you found a match')
+  }else{
+    alert('the letter was not in the word, try again')
+  }
+  }
+
+
+
+  
+
