9f25df849a920d3c1175acb5114ea29469270675
Grothusen <grothusenmichaelw@johndeere.com>
2019-12-12 12:38:28 -0600

    updated finished product

--- script.js
+++ script.js
@@ -26,68 +26,104 @@ button.addEventListener('click',captureLetter)
 let inpLetter = ""
 let clickCount = 0
 let correctCount = wordLgth
-let wrongCount = 1
+let wrongCount = 0
 let wrongLetter = 0
 let letterArr = []
 
-
 function captureLetter(e){
   let wrongLetter = 0
-  
- // console.log('this is the letter',inpLetter.value)
-  console.log(e)
+   console.log(e)
   e.preventDefault()
-  
+  document.querySelector(".message").innerHTML=" "
   inpLetter = document.querySelector("#letter").value
-
-  letterArr.push(inpLetter)
-  letterArr.forEach(checkLetter(inpLetter))
-
-  function checkLetter(letter)
+  if(inpLetter.length > 1){
+    document.querySelector(".message").innerHTML="To many letters entered, try again"
+  }
+  else{checkLetter(inpLetter)
+  }
+}
+//check if the user already click the letter.
+function checkLetter(input){
+    let n = letterArr.includes(input)
+    console.log('check to see if the array have a value',n)
+      if(letterArr.includes(input)){
+        console.log(letterArr.includes(input))
+        document.querySelector(".message").innerHTML="Letter already used, try again"
+        correctCount +=1
+        strikeLetter(inpLetter)
+      }
+      else{
+        letterArr.push(input)
+        mainGame()
+      }
+      
+}
+  console.log(letterArr)
   
 
-
+function mainGame(){
   console.log('input letter',inpLetter)
   console.log('word length',wordLgth)
   
-  for(let i = 0; i < wordLgth; i++){
-    
-    console.log(letterArr)
-    if(word.charAt(i) === inpLetter){
-        console.log(`this letter ${i} equals the input letter`)
-        document.querySelector(`[letterspace="${i}"]`).innerHTML = inpLetter
-            //document.querySelector(`[data-letter=${e.key}]`)
-        correctCount -= 1
-        wrongLetter =+1
-      }
-      console.log(correctCount)
-    if(correctCount === 0 ){
-        document.querySelector(".message").innerHTML="Yeah!!  Winner!!"
-      }
-
-      console.log(wrongLetter)
-
-    }
-      if(wrongLetter === 0){
-        let color = document.querySelector(`[count="${wrongCount}"]`)
-        color.style.opacity = 1;
-        console.log('this is the wrongCount',wrongCount)
+  if(word.includes(inpLetter)){
+          for(let i = 0; i < wordLgth; i++){
+            
+            console.log(letterArr)
+            if(word.charAt(i) === inpLetter){
+                console.log(`this letter ${i} equals the input letter`)
+                document.querySelector(`[letterspace="${i}"]`).innerHTML = inpLetter
+                    //document.querySelector(`[data-letter=${e.key}]`)
+                correctCount -= 1
+                wrongLetter =+1
+                console.log('wrongletter counter',wrongLetter)
+            }if(correctCount === 0 ){
+                document.querySelector(".message").innerHTML="Yeah!!  Winner!!"
+            }
+          }    
+          
+    }else{
         wrongCount += 1
+        let color = document.querySelector(`[count="${wrongCount}"]`)
+        color.style.opacity = 1; 
+        console.log('this is the wrongCount',wrongCount)
+        
         console.log('this is the wrongCount2',wrongCount)
         if(wrongCount > 6){
             document.querySelector(".message").innerHTML="You loose!"
           } 
-             
-        
     }
-  console.log(inpLetter)
-  let z = document.querySelector(`#${inpLetter}`)
+    strikeLetter(inpLetter)
+  }
+
+  function strikeLetter(letter){
+  console.log(letter)
+  let z = document.querySelector(`#${letter}`)
   z.style.textDecoration = "line-through" 
   clickCount += 1
   document.querySelector('#input').reset()
 
 
+  console.log('input letter',inpLetter)
+  console.log('clickcount',clickCount)
+  console.log('correct count',correctCount)
+  console.log('wrong count',wrongCount)
+  console.log('wrong letter',wrongLetter)
+  console.log('letter array',letterArr)
+
   }
+
+
+// const reloadButton = document.querySelector('.reset')
+// button.addEventListener('click',reload)
+
+//  function reload(){
+//     reload()
+  
+//  }
+
+
+
+ 
 
 
 
